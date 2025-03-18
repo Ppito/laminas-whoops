0f9e611e3a8531e74deec2318c3f2a53f0a2d9bf
Kent Rasmussen <kent@gplusmedia.com>
2025-03-18 16:29:04 +0900

    refactor: fix nullable parameter

--- src/Factory/Factory.php
+++ src/Factory/Factory.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Ppito
@@ -13,20 +14,21 @@
 namespace WhoopsErrorHandler\Factory;
 
 use Interop\Container\ContainerInterface;
-use WhoopsErrorHandler\Handler\HandlerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
+use WhoopsErrorHandler\Handler\HandlerInterface;
 
-class Factory implements FactoryInterface  {
-
+class Factory implements FactoryInterface
+{
     /**
      * Invoke Handler
      *
      * @param ContainerInterface $container
-     * @param string             $requestedName
-     * @param array|null         $options
+     * @param string $requestedName
+     * @param array|null $options
      * @return HandlerInterface
      */
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = null) {
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
+    {
         $config = $container->has('config') ? $container->get('config') : [];
         $config = $config['whoops'] ?? [];
 
